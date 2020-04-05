a210a896d26afadf99df0d50274dfd1d017b3d2a
chauhanvipin123 <vipinchauhan702@gmail.com>
2020-04-05 16:39:53 +0530

    toggle-dropdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
@@ -28,7 +28,7 @@ class App extends React.Component {
 
             if(userAuth){
               const userRef = await createUserProfileDocument(userAuth);
-              
+
               userRef.onSnapshot(snapShot=>{
                 setCurrentUser({
                   id: snapShot.id,
@@ -36,7 +36,7 @@ class App extends React.Component {
                 })
               })
             }
-            setCurrentUser({userAuth})
+            setCurrentUser(userAuth)
           
          })
 
@@ -55,7 +55,7 @@ class App extends React.Component {
       <Switch>
         <Route exact path ='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/signin'  render={()=>this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
       </Switch>
       </div>
     )
@@ -64,11 +64,15 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps =  ( { user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser : user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null, 
+  mapStateToProps, 
   mapDispatchToProps)
   (App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,11 +4,14 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import CartIcon from '../card-icon/cart-icon.component';
+
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -21,14 +24,18 @@ const Header = ({currentUser}) => (
                 (<div className='option' onClick={()=>auth.signOut()}>SIGN OUT</div>)
             
             :
-            (<Link className='option' to='/signin'>SIGN IN</Link>)
-            }
+            (<Link className='option' to='/signin'>SIGN IN</Link>
+            )}
+            <CartIcon />
         </div>
+        { hidden ? null : <CartDropdown /> }
+        
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
